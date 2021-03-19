65792069b690f9da24ee0f6c0df345abb2784514
Leo Gaskin <leo.gaskin@le0.gs>
2021-03-19 19:44:13 +0100

    Add reverse lookup to header

--- src/rhashset.h
+++ src/rhashset.h
@@ -10,5 +10,6 @@ void rhashset_grow(rhashset_t *hmap, size_t capacity);
 
 size_t rhashset_put(rhashset_t *hmap, const char *key, size_t size);
 size_t rhashset_get(rhashset_t *hmap, const char *key, size_t size);
+const char * rhashset_rev(rhashset_t *hset, size_t key);
 
 #endif
